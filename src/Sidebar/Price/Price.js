5705bb1251e82a4d46ec597b01e9b2ef9d4cfad3
ashwani5111 <ashwaniyadav7574@gmail.com>
2024-08-06 22:16:35 +0530

    Update Price.js

--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -4,41 +4,42 @@ import "./Price.css";
 const Price = ({ handleChange }) => {
   return (
     <>
-      <div className="ml">
-        <h2 className="sidebar-title price-title">Price</h2>
+      <div className="flex flex-col">
+        <h2 className="mx-5 my-2">Price</h2>
+        <div className="flex flex-col my-2 mx-5">
+          <label className="">
+            <input onChange={handleChange} type="radio" value="" name="test2" />
+            <span className=""></span>All
+          </label>
+         
+          <Input
+            handleChange={handleChange}
+            value={50}
+            title="$0 - 50"
+            name="test2"
+          />
 
-        <label className="sidebar-label-container">
-          <input onChange={handleChange} type="radio" value="" name="test2" />
-          <span className="checkmark"></span>All
-        </label>
+          <Input
+            handleChange={handleChange}
+            value={100}
+            title="$50 - $100"
+            name="test2"
+          />
 
-        <Input
-          handleChange={handleChange}
-          value={50}
-          title="$0 - 50"
-          name="test2"
-        />
+          <Input
+            handleChange={handleChange}
+            value={150}
+            title="$100 - $150"
+            name="test2"
+          />
 
-        <Input
-          handleChange={handleChange}
-          value={100}
-          title="$50 - $100"
-          name="test2"
-        />
-
-        <Input
-          handleChange={handleChange}
-          value={150}
-          title="$100 - $150"
-          name="test2"
-        />
-
-        <Input
-          handleChange={handleChange}
-          value={200}
-          title="Over $150"
-          name="test2"
-        />
+          <Input
+            handleChange={handleChange}
+            value={200}
+            title="Over $150"
+            name="test2"
+          />
+        </div>
       </div>
     </>
   );
